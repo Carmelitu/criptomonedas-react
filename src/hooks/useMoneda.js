1b7da8d36ec5074b592e7db1329e731e9f0753e9
Lucas Seijo <lucaseijo7@gmail.com>
2021-09-14 12:24:36 -0300

    se agregan estilos a select y label moneda

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,4 +1,25 @@
 import React, {Fragment, useState} from 'react';
+import styled from '@emotion/styled';
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #FFF;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2rem;
+    margin-top: 2.4rem;
+    display: block;
+`;
+
+const Select = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    font-size: 1rem;
+`;
 
 const useMoneda = (label, monedaInicial, opciones) => {
 
@@ -7,13 +28,15 @@ const useMoneda = (label, monedaInicial, opciones) => {
 
     const Seleccionar = () => (
         <Fragment>
-            <label>{label}</label>
-            <select>
+            <Label>{label}</Label>
+            <Select
+                onChange={ e => setState(e.target.value)}
+            >
                 <option value=''>-- Seleccione --</option>
                 {opciones.map(opcion => (
                     <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
                 ))}
-            </select>
+            </Select>
         </Fragment>
     );
 
